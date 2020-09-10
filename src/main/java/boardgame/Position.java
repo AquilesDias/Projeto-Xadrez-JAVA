043543eb2c90e81c0320724c85a65f8bd4e01379
Aquiles <kilesax@gmail.com>
2020-09-10 15:36:54 -0300

    First class: Position

---
+++ src/main/java/boardgame/Position.java
@@ -0,0 +1,33 @@
+package boardgame;
+
+public class Position {
+
+    private Integer row;      //LINHA
+    private Integer column;   //COLUNA
+
+
+    public Position (Integer row, Integer column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public String toString(){
+        return row +", "+ column;
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setColumn(Integer column) {
+        this.column = column;
+    }
+}
